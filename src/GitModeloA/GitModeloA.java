1ccf7097556be3e516b750e1becbcc7be9791849
mxxrioo <mario.mingorance.martinez@gmail.com>
2025-06-20 11:54:52 +0200

    Tercer Commit Master (merge)

--- src/GitModeloA/GitModeloA.java
+++ src/GitModeloA/GitModeloA.java
@@ -4,5 +4,6 @@ public class GitModeloA {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo.");
 		System.out.println("Hola a Tod@s");
+		System.out.println("Cambio en master para merge");
 	}
 }
